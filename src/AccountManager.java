10fa8092d76d6f53531d9508437d9c5475c3dbbb
K-Rey <kevbreyes@gmail.com>
2019-05-08 17:36:07 -0700

    Initial commit

---
+++ src/AccountManager.java
@@ -0,0 +1,120 @@
+/*
+Kevin Baltazar Reyes
+CSC 415
+SFSU Spring 2019
+ */
+
+import java.util.HashMap;
+import java.util.concurrent.Semaphore;
+
+public class AccountManager<K, V> {
+
+    // Class member variables.
+    Semaphore mutex = new Semaphore(1); //used for reader and write
+    Semaphore write = new Semaphore(1); //used just for writer
+    int read_count = 0;
+
+    // TODO: Implement. Use a java.util.HashMap as the underlying hash table.
+    HashMap<K, V> javaMap;
+
+    // Public methods
+
+    /**
+     * Creates a new AccountManager.
+     *
+     * @param initialSize the initial size of the table.
+     */
+    public static AccountManager create(int initialSize) {
+        return new AccountManager(initialSize);
+    }
+
+    /**
+     * Inserts a new value in the hash table.
+     */
+    public void deposit(K key, V value, int depAmount) {
+        // TODO: Implement. Writer
+        try {
+            write.acquire();
+//            System.out.println("Write semaphore acquired for insert operator");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        javaMap.put(key, value);
+        write.release();
+    }
+
+    /**
+     * Looks for a value with key k. Return null if the value does not exist.
+     */
+    public V search(K k) {
+        // TODO: Implement. Reader
+        if (javaMap.containsKey(k)) {
+            try {
+                mutex.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            read_count++;
+            if (read_count == 1) {
+                try {
+                    write.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            mutex.release();
+
+            V valueReturned = javaMap.get(k);   //reading is performed
+
+            try {
+                mutex.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            read_count--;
+            if (read_count == 0) {  //makes sure that read count gets back to zero before releasing the write semaphore
+                write.release();
+            }
+            mutex.release();
+
+            //            System.out.println("The value that is returned is: " + javaMap.get(k));
+            return valueReturned;
+        } else {
+            return null;
+        }
+
+    }
+
+    /**
+     * Deletes the value/key par for k, or does nothing if the key does not exist.
+     *
+     * @param k
+     */
+    public void delete(K k) {
+        // TODO: Implement. Writer
+        if (javaMap.containsKey(k)) {
+            try {
+                write.acquire();
+//            System.out.println("Write semaphore acquired for insert operator");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            javaMap.remove(k);
+            write.release();
+        }
+    }
+
+    // Private methods
+
+    /**
+     * Private constructor.
+     */
+    private AccountManager(int initialSize) {
+        // TODO: Implement
+        javaMap = new HashMap<K, V>(initialSize);
+
+    }
+
+
+}
